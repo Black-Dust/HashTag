8be295ae3aa494d29c1b348ffb4aacd43851d67b
Black-Dust <max.utkin91@mail.ru>
2021-03-26 18:49:58 +0300

    Добавьте файлы проекта.

---
+++ HashTag/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashTag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter tags: ");
+            string tags = Console.ReadLine();
+
+            
+            string[] outtag = tags.Split
+                    (new char[] { ';', ',' },
+                        StringSplitOptions.RemoveEmptyEntries);
+
+            
+
+
+            //char[] charsToTrimm = { ' ' };
+            char[] charsToTrimmStart = { ' ', '#' };
+            
+
+            foreach (string i in outtag)
+            {
+                //i.Removr(0);
+
+                Console.WriteLine("\"" + i.TrimStart(charsToTrimmStart) + "\"");
+
+                //bool startsWith = i.StartsWith("#");
+                /*if (i.StartsWith("#"))
+                {
+                    Console.WriteLine("\"" + i.Replace("#", "") + "\"");
+                }
+                else
+                {
+                    Console.WriteLine("\"" + i.Trim(charsToTrimm) + "\"");
+                }*/
+            }
+            Console.ReadLine();
+        }
+    }
+}
